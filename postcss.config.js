9215ac1955b1d7616235d07d676b1b8243bc6439
sss1084699875 <1622925998@qq.com>
2019-08-09 00:46:04 +0800

    处理REM的问题

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,29 @@
-module.exports = {
-  plugins: {
-    'autoprefixer': {
-      overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7']
-    },
-    'postcss-pxtorem': {
-      rootValue: 37.5,
-      propList: ['*']
+const { sep } = require('path')
+//    a/b/c
+module.exports = ({ file }) => {
+  let rootValue = file.dirname.indexOf(`node_modules${sep}vant`) !== -1
+    ? 37.5
+    : 75
+
+  return {
+    plugins: {
+      autoprefixer: {},
+      'postcss-pxtorem': {
+        rootValue,
+        propList: ['*']
+      }
     }
   }
 }
+
+// module.exports = {
+//   plugins: {
+//     'autoprefixer': {
+//       overrideBrowserslist: ['Android >= 4.0', 'iOS >= 7']
+//     },
+//     'postcss-pxtorem': {
+//       rootValue: 75,
+//       propList: ['*']
+//     }
+//   }
+// }
