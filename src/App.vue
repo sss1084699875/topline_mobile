cde192916ce586831d7585cbd539ad2edd7fcfc5
sss1084699875 <1622925998@qq.com>
2019-08-08 22:35:41 +0800

    Vant 使用

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <router-view />
   </div>
 </template>
 
 <style lang="less">
-
 </style>
