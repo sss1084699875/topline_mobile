30aadcd64fed27027d9567480fdfc21bcdbc5bd2
sss1084699875 <1622925998@qq.com>
2019-08-10 16:28:02 +0800

    导航头和标签页

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>home</div>
+  <div>
+    <!-- 导航栏 -->
+    <van-nav-bar title="黑马头条" />
+    <!-- 频道列表 -->
+    <van-tabs>
+      <van-tab
+      v-for="index in 8"
+      :title="'标签 '  + index"
+      :key="index"
+      >内容 {{ index }}</van-tab>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style lang="less" scoped>
-
 </style>
