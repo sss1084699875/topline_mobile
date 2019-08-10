2380d8563fe17535271f23f5bd875163d69356a0
sss1084699875 <1622925998@qq.com>
2019-08-10 16:51:17 +0800

    list组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,64 @@
 <template>
   <div>
     <!-- 导航栏 -->
-    <van-nav-bar title="黑马头条" />
+    <van-nav-bar title="黑马头条"
+     />
     <!-- 频道列表 -->
     <van-tabs>
       <van-tab
       v-for="index in 8"
       :title="'标签 '  + index"
-      :key="index"
-      >内容 {{ index }}</van-tab>
+      :key="index">
+      <!-- 内容 {{ index }} -->
+      <!-- 文章列表 不同的频道有不同的文章列表 -->
+       <!-- 当lsit没有到达页面底部的时候,会触发load事件,并吧loading设置为true -->
+        <!-- loading 为true 会显示正在加载 当数据加载完毕应该吧loading设置为false -->
+      <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+        >
+        <van-cell
+            v-for="item in list"
+            :key="item"
+            :title="item"
+        />
+        </van-list>
+      </van-tab>
     </van-tabs>
+
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // list 文章列表的数据
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
