2a2a382289cabd1334e1f618578f87d42ca13bd1
sss1084699875 <1622925998@qq.com>
2019-08-13 21:07:18 +0800

    MoreAction中显示的内容

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -8,14 +8,32 @@
         :showConfirmButton = 'false'
         closeOnClickOverlay
         >
-        hell
+        <van-cell-group v-show="!isReportShow">
+          <van-cell title="不感兴趣"  icon="bill-o" />
+          <van-cell title="反馈垃圾内容" icon="warning-o" is-link @click="isReportShow=!isReportShow"/>
+          <van-cell title="拉黑作者"  icon="delete"/>
+        </van-cell-group>
+
+        <van-cell-group v-show="isReportShow">
+          <van-cell icon="arrow-left" @click="isReportShow =!isReportShow"/>
+          <van-cell title="标题夸张"/>
+          <van-cell title="低俗色情"/>
+          <van-cell title="错别字多"/>
+        </van-cell-group>
+
         </van-dialog>
 </template>
 
 <script>
 export default {
   name: 'MoreAction',
-  props: ['value']
+  props: ['value'],
+  data () {
+    return {
+      // 控制反馈垃圾内容的cell是否显示
+      isReportShow: false
+    }
+  }
 
 }
 </script>
