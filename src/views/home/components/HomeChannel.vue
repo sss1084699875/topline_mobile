1c05098ded685a8a6c9be5110849239b22a2125c
sss1084699875 <1622925998@qq.com>
2019-08-14 22:41:56 +0800

    频道管理-推荐频道-把全部频道中我的频道的内容过滤

--- src/views/home/components/HomeChannel.vue
+++ src/views/home/components/HomeChannel.vue
@@ -25,9 +25,9 @@
            <van-button
             type="danger"
             plain
+            size="mini"
             v-show="showClose"
             @click="showClose = false"
-            size="mini"
           >完成</van-button>
         </div>
       </div>
@@ -52,7 +52,7 @@
       <!-- 推荐频道列表 -->
       <van-grid class="channel-content" :gutter="10" clickable>
         <van-grid-item
-          v-for="item in allChannels"
+          v-for="item in recommendChannels"
           :key="item.id">
           <div class="info">
             <span class="text">{{item.name}}</span>
@@ -67,7 +67,19 @@
 import { getAllChannels } from '@/api/channel'
 export default {
   name: 'HomeChannel',
-  props: [ 'value', 'channels', 'activeIndex' ],
+  props: ['value', 'channels', 'activeIndex'],
+  computed: {
+    recommendChannels () {
+      // 我的频道的所有id
+      const ids = this.channels.map((item) => {
+        return item.id
+      })
+
+      return this.allChannels.filter((item) => {
+        return !ids.includes(item.id)
+      })
+    }
+  },
   data () {
     return {
       showClose: false,
