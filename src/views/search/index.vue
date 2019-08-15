67c828350ff0866cd78a7a2258de2e3128fa00cb
sss1084699875 <1622925998@qq.com>
2019-08-15 17:39:18 +0800

    函数防抖解决频繁请求的问题

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -17,6 +17,7 @@
 </template>
 
 <script>
+import _ from 'lodash'
 import { getSuggestion } from '@/api/search'
 export default {
   data () {
@@ -31,7 +32,7 @@ export default {
       console.log('sss')
     },
     // 用户在文本框输入内容的时候,获取搜索建议
-    async  handleSuggestion () {
+    handleSuggestion: _.debounce(async function () {
       if (this.value.trim() === '') {
         this.suggestionList = []
         return
@@ -44,7 +45,7 @@ export default {
       } catch (err) {
         console.log(err)
       }
-    }
+    }, 500)
   }
 }
 </script>
