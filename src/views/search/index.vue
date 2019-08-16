fc840fcf4ab70db97dd9eb6a8f6dad440bf82d32
sss1084699875 <1622925998@qq.com>
2019-08-16 21:39:32 +0800

    搜索功能-历史记录的界面

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,8 +11,33 @@
     <!-- 搜索建议 -->
     <van-cell-group>
     <van-cell  v-for="item in suggestionList" :key="item" :title="item" icon="search"  />
-
     </van-cell-group>
+
+    <!-- 历史记录 -->
+     <van-cell-group>
+    <van-cell title="历史记录">
+      <van-icon
+      v-show="!showClose"
+      @click="showClose = true"
+      slot="right-icon"
+      name="delete"
+      style="line-height: inherit;"
+      />
+      <div v-show="showClose">
+        <span>全部删除</span>&nbsp;
+        <span @click="showClose = false">完成</span>
+      </div>
+    </van-cell>
+    <van-cell title="hello">
+      <van-icon
+      v-show="showClose"
+      slot="right-icon"
+      name="close"
+      style="line-height: inherit;"
+      />
+    </van-cell>
+    </van-cell-group>
+
 </div>
 </template>
 
@@ -24,7 +49,9 @@ export default {
     return {
       value: '',
       // 存储搜索建议的列表
-      suggestionList: []
+      suggestionList: [],
+      // 控制关闭的显示和隐藏
+      showClose: false
     }
   },
   methods: {
