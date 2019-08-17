d45ccefe032772c83deaa38c5fc0effc8c6bbdec
sss1084699875 <1622925998@qq.com>
2019-08-17 21:05:49 +0800

    搜索功能-搜索结果界面

--- src/views/search-result/index.vue
+++ src/views/search-result/index.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-        搜索
+      <van-nav-bar
+        title="搜索结果"
+        left-text="返回"
+        fixed
+        left-arrow
+        @click-left="$router.back()"
+        />
+        <!-- 列表 -->
+        <van-list
+            v-model="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+            >
+            <van-cell
+                v-for="item in list"
+                :key="item"
+                :title="item"
+            />
+        </van-list>
     </div>
 </template>
 
@@ -8,14 +27,36 @@
 export default {
   name: 'SearchResult',
   props: ['q'],
-  created () {
-    // route 路由数据  //params 动态路由参数
-    // console.log(this.$route.params.q)
-    console.log(this.q)
+  data () {
+    return {
+      list: [],
+      loading: false,
+      finished: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true
+        }
+      }, 500)
+    }
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
+.van-list {
+    margin-top: 80px;
+}
 </style>
