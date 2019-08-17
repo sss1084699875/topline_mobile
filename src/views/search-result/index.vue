e0da2ca2b73cb194d07bcabfd01ef74d0ce078ae
sss1084699875 <1622925998@qq.com>
2019-08-17 10:47:48 +0800

    搜索功能-搜索结果页面-路由传参

---
+++ src/views/search-result/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        搜索
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResult',
+  props: ['q'],
+  created () {
+    // route 路由数据  //params 动态路由参数
+    // console.log(this.$route.params.q)
+    console.log(this.q)
+  }
+}
+</script>
+
+<style>
+
+</style>
