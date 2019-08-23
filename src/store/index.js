49990934ef0aeaac507d56f94cab01abf0c107e2
sss1084699875 <1622925998@qq.com>
2019-08-23 10:33:52 +0800

    文章详情-点击回复弹出replylist

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,22 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     // 登录状态信息
-    user: getUser()
+    user: getUser(),
+    // 控制replyList弹出层显示隐藏
+    showReplyList: false,
+    // 点击回复按钮的时候,记录当前评论对象
+    currentComment: null
   },
   mutations: {
     setUser (state, data) {
       state.user = data
       setUser(data)
+    },
+    setShowReplyList (state, value) {
+      state.showReplyList = value
+    },
+    setCurrentComment (state, value) {
+      state.currentComment = value
     }
   },
   actions: {
